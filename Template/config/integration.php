47eb642abc526309a99e776c0e174fe7b8cc4dcf
abdullah09i <156966742+abdullah09i@users.noreply.github.com>
2024-01-18 05:36:23 +0300

    Update integration.php

--- Template/config/integration.php
+++ Template/config/integration.php
@@ -1,9 +1,9 @@
 <h3><i class="fa fa-slack fa-fw"></i>Slack</h3>
 <div class="panel">
     <?= $this->form->label(t('Webhook URL'), 'slack_webhook_url') ?>
-    <?= $this->form->text('slack_webhook_url', $values) ?>
 
-    <p class="form-help"><a href="https://github.com/kanboard/plugin-slack#configuration" target="_blank"><?= t('Help on Slack integration') ?></a></p>
+
+    <p class="form-help"><a https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks" target="_blank"><?= t('Help communicating with Discord') ?></a></p>
 
     <div class="form-actions">
         <input type="submit" value="<?= t('Save') ?>" class="btn btn-blue"/>
